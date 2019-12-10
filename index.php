1051d0164497eb212a106496ba02f3fb26a30db4
savickasp <savickas.p@gmail.com>
2019-12-10 13:39:34 +0200

    form

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+if (isset($_POST['form'])) {
+    $inputArr = $_POST['form'];
+}
+
+?>
+<html>
+<head>
+    <title>Class_work</title>
+</head>
+<body>
+<form method="post">
+    <input type="text" name="form[name]" placeholder="Vardas" required>
+    <input type="text" name="form[surname]" placeholder="Pavarde" required>
+    <input type="number" name="form[age]" placeholder="Age">
+    <select name="form[level]">
+        <option value="1" selected>Pradedantysis</option>
+        <option value="2">Pazenges</option>
+        <option value="3">Profesionalas</option>
+    </select>
+    <button>Submit</button>
+</form>
+<section>
+    <?php foreach ($inputArr as $key => $input): ?>
+    <div><?php printf('%s: %s', $key, $input );?></div>
+    <?php endforeach;?>
+</section>
+</body>
+</html>
