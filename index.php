b4aafc8bede7f4819cd049722e7a8769e33d53f1
savickasp <savickas.p@gmail.com>
2019-12-10 14:53:16 +0200

    form changes photo size

--- index.php
+++ index.php
@@ -1,35 +1,23 @@
 <?php
 
-$h2 = 'Ducho sertifikatas';
-
-if (isset($_POST['form'])) {
-    $inputArr = $_POST['form'];
-} else {
-    $inputArr = [];
-}
-
+$url_car = 'https://media.wired.com/photos/5d09594a62bcb0c9752779d9/master/pass/Transpo_G70_TA-518126.jpg';
+$vh = $_POST['size'] ?? 30;
 ?>
 <html>
 <head>
     <title>Class_work</title>
+    <style>
+        .car {
+            height: <?php print $vh; ?>vh;
+            object-fit: contain;
+        }
+    </style>
 </head>
 <body>
 <form method="post">
-    <input type="text" name="form[name]" placeholder="Vardas" required>
-    <input type="text" name="form[surname]" placeholder="Pavarde" required>
-    <input type="number" name="form[age]" placeholder="Age">
-    <select name="form[level]">
-        <option value="1" selected>Pradedantysis</option>
-        <option value="2">Pazenges</option>
-        <option value="3">Profesionalas</option>
-    </select>
+    <input name="size" type="range" min="1" max="90" value="<?php print $vh; ?>">
     <button>Submit</button>
 </form>
-<section>
-    <h2><?php print $h2;?></h2>
-    <?php foreach ($inputArr as $key => $input): ?>
-    <div><?php printf('%s: %s', $key, $input );?></div>
-    <?php endforeach;?>
-</section>
+<img class="car" src="<?php print $url_car; ?>">
 </body>
 </html>
